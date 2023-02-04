6743721d197f78e77d2f6a10db689fee133a4598
Toby <toby_van_mook@hotmail.com>
2023-02-04 20:31:23 +0000

    added components

--- MutualGrain/App.js
+++ MutualGrain/App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View, Image } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Image source={require("./assets/logo.png")} />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Image source={require("./assets/logo.png")} />
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
