7bfdf5e1d43bb2a75ca8ea8cbb2e12f74c935ab1
Rishabh Sharma <rs2522@ic.ac.uk>
2023-02-05 01:02:12 +0000

    i want to die

--- MutualGrain/App.js
+++ MutualGrain/App.js
@@ -2,27 +2,59 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View, Image } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createAppContainer } from "react-navigation";
+import { createMaterialBottomTabNavigator } from
+    "react-navigation-material-bottom-tabs";
 import Home from "./pages/Home";
 import Search from "./pages/Search";
 
 
-const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <View style={styles.container}>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Search" component={Search} />
-      </Tab.Navigator>
-        <StatusBar style="auto" />
-      </View>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Navigator>
+        <Home />
+        <Search/>
+      </Navigator>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
+const TabNavigator = createMaterialBottomTabNavigator(  
+  {  
+      Home: { screen: Home,  
+          navigationOptions:{  
+              tabBarLabel:'Home',  
+              tabBarIcon: ({ tintColor }) => (  
+                  <View>  
+                      <Icon style={[{color: tintColor}]} size={25} name={'ios-home'}/>  
+                  </View>),  
+          }  
+      },  
+      Search: { screen: Search,  
+          navigationOptions:{  
+              tabBarLabel:'Profile',  
+              tabBarIcon: ({ tintColor }) => (  
+                  <View>  
+                      <Icon style={[{color: tintColor}]} size={25} name={'ios-search'}/>  
+                  </View>),  
+              activeColor: '#f60c0d',  
+              inactiveColor: '#f65a22',  
+              barStyle: { backgroundColor: '#f69b31' },  
+          }  
+      }
+    },  
+    {  
+      initialRouteName: "Home",  
+      activeColor: '#f0edf6',  
+      inactiveColor: '#226557',  
+      barStyle: { backgroundColor: '#3BAD87' },  
+    },  
+)
 
+const Navigator = createAppContainer(TabNavigator);
 
 
 function MyTabs() {
--- MutualGrain/pages/Search.js
+++ MutualGrain/pages/Search.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Image } from "react-native";
+import { StyleSheet, View, Image, SafeAreaView } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import MapView from 'react-native-maps';
 import * as Location from 'expo-location';
@@ -7,17 +7,15 @@ import * as Location from 'expo-location';
 
 const Search = () => {
   return (
-    <NavigationContainer>
-      <View style={styles.container}>
-        <MapView 
-         style = {{height : '100%', width : '100%'}}
-         coordinate = {Location.getLastKnownPositionAsync}
-         region = {{latitudeDelta: 0.0922,
-         longitudeDelta: 0.0421}}/>
-        
-        <StatusBar style="auto" />
-      </View>
-    </NavigationContainer>
+    <SafeAreaView style={styles.container}>
+      <MapView 
+        style = {{height : '100%', width : '100%'}}
+        coordinate = {Location.getLastKnownPositionAsync}
+        region = {{latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421}}/>
+      
+      <StatusBar style="auto" />
+    </SafeAreaView>
   );
 }
 
