8f6629bd0322e7ed379b6df4e2102103a79074a2
Toby <toby_van_mook@hotmail.com>
2023-02-05 01:19:10 +0000

    stuff

--- MutualGrain/pages/Search.js
+++ MutualGrain/pages/Search.js
@@ -1,25 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View, Image } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import MapView from 'react-native-maps';
-import * as Location from 'expo-location';
-
+import MapView from "react-native-maps";
+import * as Location from "expo-location";
 
 const Search = () => {
   return (
-    <NavigationContainer>
-      <View style={styles.container}>
-        <MapView 
-         style = {{height : '100%', width : '100%'}}
-         coordinate = {Location.getLastKnownPositionAsync}
-         region = {{latitudeDelta: 0.0922,
-         longitudeDelta: 0.0421}}/>
-        
-        <StatusBar style="auto" />
-      </View>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <MapView
+        style={{ height: "100%", width: "100%" }}
+        coordinate={Location.getLastKnownPositionAsync}
+        region={{ latitudeDelta: 0.0922, longitudeDelta: 0.0421 }}
+      />
+
+      <StatusBar style="auto" />
+    </View>
   );
-}
+};
 
 export default Search;
 
