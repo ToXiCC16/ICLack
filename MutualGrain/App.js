415a31b9160303e953ec827f633d7662f59ac0aa
Rishabh Sharma <rs2522@ic.ac.uk>
2023-02-05 01:20:05 +0000

    failed tab navigator implementation

--- MutualGrain/App.js
+++ MutualGrain/App.js
@@ -3,11 +3,51 @@ import { StyleSheet, View, Image } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createAppContainer } from "react-navigation";
+import { Ionicons } from "@expo/vector-icons";
 import { createMaterialBottomTabNavigator } from
     "react-navigation-material-bottom-tabs";
 import Home from "./pages/Home";
 import Search from "./pages/Search";
 
+
+
+
+const TabNavigator = createMaterialBottomTabNavigator(  
+  {
+    Home: {
+      screen: Home,
+      navigationOptions: {
+        tabBarLabel: "Home",
+        tabBarIcon: (tabInfo) => (
+          <Ionicons
+            name="md-home"
+            size={tabInfo.focused ? 26 : 20}
+            color={tabInfo.tintColor}
+          />
+        ),
+      },
+    },
+    Search: {
+      screen: Search,
+      navigationOptions: {
+        tabBarLabel: "Search",
+        tabBarIcon: (tabInfo) => (
+          <Ionicons
+            name="md-search"
+            size={tabInfo.focused ? 26 : 20}
+            color={tabInfo.tintColor}
+          />
+        ),
+      },
+    },
+  },
+  {
+    initialRouteName: "Home",
+    barStyle: { backgroundColor: "#006600" },
+  }
+);
+
+const Navigator = createAppContainer(TabNavigator);
 
 
 export default function App() {
@@ -22,49 +62,15 @@ export default function App() {
   );
 }
 
-const TabNavigator = createMaterialBottomTabNavigator(  
-  {  
-      Home: { screen: Home,  
-          navigationOptions:{  
-              tabBarLabel:'Home',  
-              tabBarIcon: ({ tintColor }) => (  
-                  <View>  
-                      <Icon style={[{color: tintColor}]} size={25} name={'ios-home'}/>  
-                  </View>),  
-          }  
-      },  
-      Search: { screen: Search,  
-          navigationOptions:{  
-              tabBarLabel:'Profile',  
-              tabBarIcon: ({ tintColor }) => (  
-                  <View>  
-                      <Icon style={[{color: tintColor}]} size={25} name={'ios-search'}/>  
-                  </View>),  
-              activeColor: '#f60c0d',  
-              inactiveColor: '#f65a22',  
-              barStyle: { backgroundColor: '#f69b31' },  
-          }  
-      }
-    },  
-    {  
-      initialRouteName: "Home",  
-      activeColor: '#f0edf6',  
-      inactiveColor: '#226557',  
-      barStyle: { backgroundColor: '#3BAD87' },  
-    },  
-)
 
-const Navigator = createAppContainer(TabNavigator);
-
-
-function MyTabs() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Search" component={Search} />
-    </Tab.Navigator>
-  );
-}
+// function MyTabs() {
+//   return (
+//     <Tab.Navigator>
+//       <Tab.Screen name="Home" component={Home} />
+//       <Tab.Screen name="Search" component={Search} />
+//     </Tab.Navigator>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
