41bccb520042bbfe1a38b7588469efe9a1f517a3
Toby <toby_van_mook@hotmail.com>
2023-02-05 03:44:22 +0000

    mongodb stuff

--- MutualGrain/pages/Search.js
+++ MutualGrain/pages/Search.js
@@ -1,66 +1,66 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Image, SafeAreaView } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { Constants, MapView, Location, Permissions } from 'expo';
-import React, { useState, useEffect} from 'react';
+// import { StatusBar } from "expo-status-bar";
+// import { StyleSheet, View, Image, SafeAreaView } from "react-native";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { Constants, MapView, Location, Permissions } from 'expo';
+// import React, { useState, useEffect} from 'react';
 
-const Search = () => {
-  state = {
-    mapRegion: null,
-    hasLocationPermissions: false,
-    locationResult: null
-  };
+// const Search = () => {
+//   state = {
+//     mapRegion: null,
+//     hasLocationPermissions: false,
+//     locationResult: null
+//   };
 
-  componentDidMount() {
-    this._getLocationAsync();
-  }
+//   componentDidMount() {
+//     this._getLocationAsync();
+//   }
 
-  _handleMapRegionChange = mapRegion => {
-    console.log(mapRegion);
-    this.setState({ mapRegion });
-  };
+//   _handleMapRegionChange = mapRegion => {
+//     console.log(mapRegion);
+//     this.setState({ mapRegion });
+//   };
 
-  _getLocationAsync = async () => {
-   let { status } = await Permissions.askAsync(Permissions.LOCATION);
-   if (status !== 'granted') {
-     this.setState({
-       locationResult: 'Permission to access location was denied',
-     });
-   } else {
-     this.setState({ hasLocationPermissions: true });
-   }
+//   _getLocationAsync = async () => {
+//    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+//    if (status !== 'granted') {
+//      this.setState({
+//        locationResult: 'Permission to access location was denied',
+//      });
+//    } else {
+//      this.setState({ hasLocationPermissions: true });
+//    }
 
-   let location = await Location.getCurrentPositionAsync({});
-   this.setState({ locationResult: JSON.stringify(location) });
-   
-   // Center the map on the location we just fetched.
-    this.setState({mapRegion: { latitude: location.coords.latitude, longitude: location.coords.longitude, latitudeDelta: 0.0922, longitudeDelta: 0.0421 }});
-  };
+//    let location = await Location.getCurrentPositionAsync({});
+//    this.setState({ locationResult: JSON.stringify(location) });
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <MapView
-              style={{ alignSelf: 'stretch', height: 400 }}
-              region={this.state.mapRegion}
-              onRegionChange={this._handleMapRegionChange}
-            >
-          {userLocation && <Marker coordinate={userLocation.coords} />}
-        </MapView>
+//    // Center the map on the location we just fetched.
+//     this.setState({mapRegion: { latitude: location.coords.latitude, longitude: location.coords.longitude, latitudeDelta: 0.0922, longitudeDelta: 0.0421 }});
+//   };
 
-        <StatusBar style="auto" />
-      </View>
-    );
-  }
-};
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <MapView
+//               style={{ alignSelf: 'stretch', height: 400 }}
+//               region={this.state.mapRegion}
+//               onRegionChange={this._handleMapRegionChange}
+//             >
+//           {userLocation && <Marker coordinate={userLocation.coords} />}
+//         </MapView>
 
-export default Search;
+//         <StatusBar style="auto" />
+//       </View>
+//     );
+//   }
+// };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+// export default Search;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
